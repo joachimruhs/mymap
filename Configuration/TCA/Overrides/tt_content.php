d8d7865a0e9993c612daed94de59284024aed9a9
Joachim Ruhs <postmaster@joachim-ruhs.de>
2019-12-24 09:29:30 +0100

    TCA migrations
    moves plugin registrations to TCA/Overrides/tt_content.php

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,80 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+/*********
+ * Plugins
+ */
+
+$_EXTKEY = 'mymap';
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+	$_EXTKEY,
+	'Searchform',
+	'MYMAP - Search Form'
+);
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+	$_EXTKEY,
+	'Autocompleterform',
+	'MYMAP - Autocompleter Form'
+);
+
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+	$_EXTKEY,
+	'Search',
+	'MYMAP - Search'
+);
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+	$_EXTKEY,
+	'MapAll',
+	'MYMAP - Map with all locations'
+);
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+	$_EXTKEY,
+	'Ajaxsearch',
+	'MYMAP - Ajaxsearch'
+);
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+	$_EXTKEY,
+	'SingleView',
+	'MYMAP - Single View'
+);
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+	$_EXTKEY,
+	'RandomView',
+	'MYMAP - Random View'
+);
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+	$_EXTKEY,
+	'AjaxSearch',
+	'MYMAP - Ajax Search'
+);
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+/*
+ 
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['news_pi1'] = 'recursive,select_key,pages';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['news_pi1'] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('news_pi1',
+    'FILE:EXT:news/Configuration/FlexForms/flexform_news.xml');
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToInsertRecords('tx_news_domain_model_news');
+*/
--- ext_tables.php
+++ ext_tables.php
@@ -1,62 +1,16 @@
 <?php
+
 if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Searchform',
-	'MYMAP - Search Form'
-);
+$boot = function () {
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Autocompleterform',
-	'MYMAP - Autocompleter Form'
-);
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_mymap_domain_model_location', 'EXT:mymap/Resources/Private/Language/locallang_csh_tx_mymap_domain_model_location.xlf');
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_mymap_domain_model_location');
 
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Search',
-	'MYMAP - Search'
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'MapAll',
-	'MYMAP - Map with all locations'
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Ajaxsearch',
-	'MYMAP - Ajaxsearch'
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'SingleView',
-	'MYMAP - Single View'
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'RandomView',
-	'MYMAP - Random View'
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'AjaxSearch',
-	'MYMAP - Ajax Search'
-);
-
-
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_mymap_domain_model_location', 'EXT:mymap/Resources/Private/Language/locallang_csh_tx_mymap_domain_model_location.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_mymap_domain_model_location');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_mymap_domain_model_category', 'EXT:mymap/Resources/Private/Language/locallang_csh_tx_mymap_domain_model_category.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_mymap_domain_model_category');
-
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_mymap_domain_model_category', 'EXT:mymap/Resources/Private/Language/locallang_csh_tx_mymap_domain_model_category.xlf');
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_mymap_domain_model_category');
+};
+$boot();
+unset($boot);
